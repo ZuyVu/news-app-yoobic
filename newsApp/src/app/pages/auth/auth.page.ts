6770b19d6e9b9a71a505e0f380f519084aba7321
Duy Vu <dtv15@imperial.co.uk>
2019-12-05 20:14:58 +0000

    Implemented auth.page

--- newsApp/src/app/pages/auth/auth.page.ts
+++ newsApp/src/app/pages/auth/auth.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLogin = true;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.authService.login();
+    // Navigate user to movies page
+    this.router.navigateByUrl('/movies');
+  }
+
+  onSubmit(form: NgForm) {
+    // console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLogin) {
+      // Send Login request
+    } else {
+      // Send Signup request
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
