3b27d07584161e2921c3194b41037edfda90446f
Duy Vu <dtv15@imperial.co.uk>
2019-12-05 20:23:45 +0000

    Implemented news service

--- newsApp/src/app/services/news.service.ts
+++ newsApp/src/app/services/news.service.ts
@@ -4,6 +4,40 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class NewsService {
-
+  // private url = 'https://newsapi.org/v2/top-headlines';
+  // private key = ;
+  private data = [];
   constructor() { }
+
+  fetchHeadlinesNews(country: string) {
+    const url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=f7368915fb624144b95da6ee35409843';
+    const req = new Request(url);
+    fetch(req)
+        .then((response) => response.json())
+        .then(data => {
+          this.setData(data.articles);
+          return data.articles;
+        });
+  }
+
+  setData(data: any) {
+    this.data = data;
+  }
+ 
+  getData(id) {
+    // return this.data[id];
+    return this.search(id, this.data);
+  }
+
+  search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].url === nameKey) {
+            return myArray[i];
+        }
+    }
+}
+
+
 }
