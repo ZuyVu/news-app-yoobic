ad75582b29af3be6088cb1575bb069e54a9b3ef0
Duy Vu <dtv15@imperial.co.uk>
2019-12-05 21:20:28 +0000

    Implemented article page; Service not yet provide infor for article

--- newsApp/src/app/pages/article/article.page.ts
+++ newsApp/src/app/pages/article/article.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.page.scss'],
 })
 export class ArticlePage implements OnInit {
+  article = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private newsService: NewsService) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.article = this.newsService.getData(id);
   }
-
 }
