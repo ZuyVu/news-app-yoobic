3f71cfcfd504c1977fd6c94ddec64af5e5405d27
Duy Vu <dtv15@imperial.co.uk>
2019-12-05 20:07:25 +0000

    Auto generate necessary pages, services and guard

--- newsApp/src/app/app-routing.module.ts
+++ newsApp/src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'headlines',
+    loadChildren: () => import('./pages/headlines/headlines.module').then( m => m.HeadlinesPageModule)
+  },
+  {
+    path: 'article',
+    loadChildren: () => import('./pages/article/article.module').then( m => m.ArticlePageModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthPageModule)
+  },
 ];
 
 @NgModule({
