5c479378138444ac4074cef62b6f98ab4fde86e7
Duy Vu <dtv15@imperial.co.uk>
2019-12-05 20:17:46 +0000

    Implemented authorisation guard

--- newsApp/src/app/guards/auth.guard.ts
+++ newsApp/src/app/guards/auth.guard.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanLoad {
+
+  constructor(private authSerivce: AuthService, private router: Router) {}
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authSerivce.authStatus) {
+      // If not authorised, navigate back to login page
+      this.router.navigateByUrl('/auth');
+    }
+
+    return this.authSerivce.authStatus;
+
+  }
+
 }
