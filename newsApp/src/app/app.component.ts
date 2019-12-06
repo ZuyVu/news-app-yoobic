1fade648906684493140bfe59787e0aa9bc57efd
Duy Vu <dtv15@imperial.co.uk>
2019-12-06 00:42:49 +0000

    Implemented side panel

--- newsApp/src/app/app.component.ts
+++ newsApp/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,10 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private alertCtr: AlertController,
+    private authService: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -24,4 +29,25 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  onLogOut() {
+    console.log('LOGOUT IS CLICKED!!!!');
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
+  }
+
+  async onClickDummyButton() {
+    console.log('Dummy button is clicked');
+
+    const alert = await this.alertCtr.create({
+      header: 'Feature not Implemented',
+      message: 'This is a just a dummy button.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+  }
+
+
 }
